d4e444b80eb97389ca957f7248a06affb405a904
hashirbutt1979 <ndakhurram@gmail.com>
2025-06-25 19:33:05 +0500

    lesson22isdone

---
+++ lesson22/demo.js
@@ -0,0 +1,18 @@
+var b = "heellooooo"
+var hiba={
+
+    student : () =>{
+        console.log("hey whatsup")
+        console.log("how are you ")
+    },
+
+    a:66,
+    b:"beauty lie in the eyes of beholder"
+    
+}
+
+hiba.student()
+console.log(hiba.b)
+
+console.log(Math.random())
+console.log(Math.PI)
